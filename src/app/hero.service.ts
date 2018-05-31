331d295dcc620c8dd98858328d85f3b765c1ed12
INOSTUDIO\a.kostin <artem.kostin@gmail.com>
2018-05-31 16:09:29 +0300

    add HeroService class

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+// register the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  // simulate getting data from the server with the RxJS of() function
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
---
+++ src/app/message.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+// create an injectable, app-wide MessageService for sending messages to be displayed
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  messages: string[] = [];
+
+  constructor() { }
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
